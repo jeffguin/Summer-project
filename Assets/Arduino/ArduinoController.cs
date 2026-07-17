7e559666bcb1765b291441aa85ef0d4ee327aeef
Nicholas Cornwell <115149463+heyitsnick4@users.noreply.github.com>
2026-07-17 17:16:40 +0100

    Added arduino connection

---
+++ Assets/Arduino/ArduinoController.cs
@@ -0,0 +1,75 @@
+using UnityEngine;
+using System.IO.Ports;
+
+public class ArduinoController : MonoBehaviour
+{
+    SerialPort serial = new SerialPort("COM4", 9600);
+
+    [SerializeField] private GameObject sweetPrefab;
+    [SerializeField] private Transform spawnPoint;
+
+    void Start()
+    {
+        serial.Open();
+        serial.ReadTimeout = 50;
+    }
+
+
+    void Update()
+    {
+        // Unity > Arduino
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            serial.Write("1");
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            serial.Write("2");
+        }
+
+
+        // Arduino > Unity
+
+        if (serial.IsOpen)
+        {
+            try
+            {
+                string message = serial.ReadLine();
+
+                if (message == "BUTTON_PRESS")
+                {
+                    ArduinoButtonPressed();
+                }
+            }
+            catch
+            {
+
+            }
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Sweet"))
+        {
+            serial.Write("1");
+            Destroy(other.gameObject);
+        }
+    }
+
+    void ArduinoButtonPressed()
+    {
+        Debug.Log("Arduino button pressed!");
+        Instantiate(sweetPrefab, spawnPoint.position, Quaternion.identity);
+    }
+
+
+    void OnApplicationQuit()
+    {
+        if (serial.IsOpen)
+            serial.Close();
+    }
+}
