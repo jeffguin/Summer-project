ca93ecb5742a2d5213a4c05d1b1991cc49f709d4
Jeffrey P <jefferypoon03@gmail.com>
2026-07-26 22:29:59 +0100

    Map + failed avatar

--- Assets/Scripts/Summer/PhysicalWindowSize.cs
+++ Assets/Scripts/Summer/PhysicalWindowSize.cs
@@ -5,13 +5,22 @@ using UnityEngine;
 public class PhysicalWindowSize : MonoBehaviour
 {
     [Header("Window Size (meters)")]
-    //home default value 60cmx30cm
-    public float width = 0.6f;  
-    public float height = 0.3f; 
+    // Home default value: 60cm x 30cm
+    public float width = 0.6f;
+    public float height = 0.3f;
+
+    [Header("Ground Placement")]
+    public GameObject ground;
+
+    [Tooltip("Distance between the lowest point of the quad and the ground.")]
+    public float heightAboveGround = 0.1f;
+
+    private Collider groundCollider;
 
     void Update()
     {
         UpdateWindowScale();
+        UpdateWindowHeight();
     }
 
     void UpdateWindowScale()
@@ -19,20 +28,51 @@ public class PhysicalWindowSize : MonoBehaviour
         transform.localScale = new Vector3(width, height, 1f);
     }
 
+    void UpdateWindowHeight()
+    {
+        if (ground == null)
+            return;
 
-    public Vector3 GetBottomLeft() 
+        groundCollider = ground.GetComponent<Collider>();
+
+        if (groundCollider == null)
+        {
+            Debug.LogWarning("Ground object does not have a Collider.");
+            return;
+        }
+
+        // Get the lowest point of the quad in world space
+        Vector3 bottomLeft = GetBottomLeft();
+        Vector3 bottomRight = GetBottomRight();
+
+        float lowestY = Mathf.Min(bottomLeft.y, bottomRight.y);
+
+        // Get the top surface of the ground
+        float groundTopY = groundCollider.bounds.max.y;
+
+        // Move the entire window so its lowest point is the desired distance above ground
+        float requiredY = groundTopY + heightAboveGround;
+        float offsetY = requiredY - lowestY;
+
+        transform.position += new Vector3(0f, offsetY, 0f);
+    }
+
+    public Vector3 GetBottomLeft()
     {
         return transform.TransformPoint(new Vector3(-0.5f, -0.5f, 0));
     }
-    public Vector3 GetBottomRight() 
+
+    public Vector3 GetBottomRight()
     {
         return transform.TransformPoint(new Vector3(0.5f, -0.5f, 0));
     }
-    public Vector3 GetTopLeft() 
+
+    public Vector3 GetTopLeft()
     {
         return transform.TransformPoint(new Vector3(-0.5f, 0.5f, 0));
     }
-    public Vector3 GetTopRight() 
+
+    public Vector3 GetTopRight()
     {
         return transform.TransformPoint(new Vector3(0.5f, 0.5f, 0));
     }
