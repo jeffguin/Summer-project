115296b923f47c0db127dad0c305d75f81ec9753
Yuxiang Jin <yuxiangjin250028@gmail.com>
2026-08-03 17:50:06 +0100

    still fail

--- Assets/Scripts/Summer/EyeLocationSimulation.cs
+++ Assets/Scripts/Summer/EyeLocationSimulation.cs
@@ -1,32 +1,10 @@
 using UnityEngine;
-using Valve.VR;
+
 
 public class EyeLocationSimulation : MonoBehaviour
 {
     public Transform target;
     public Vector3 offset;
-
-
-    /// /////////
-
-    void Start()
-    {
-        SteamVR_Behaviour_Pose[] trackers =
-            FindObjectsByType<SteamVR_Behaviour_Pose>(
-                FindObjectsSortMode.None);
-
-        foreach (var tracker in trackers)
-        {
-            Debug.Log(tracker.gameObject.name);
-        }
-    }
-
-    /// /////////////
- 
-
-
-
-
 
     void LateUpdate()
     {
